bdacd6e291fd493424a8aa0b415dbfc76bf32355
MarceeW <marcelllkadar@gmail.com>
2024-07-25 21:54:13 +0200

    Updated login component

--- client/src/app/account/login/login.component.ts
+++ client/src/app/account/login/login.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { LoginModel } from '../../_models/login-model';
 import { AsyncPipe } from '@angular/common';
 import { AccountService } from '../../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
 import { Router } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormErrorStateMatcher } from '../../_state-matchers/form-error-state-matcher';
 
 @Component({
   selector: 'app-login',
@@ -12,21 +15,29 @@ import { Router } from '@angular/router';
   imports: [
     FormsModule,
     AsyncPipe,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-  model : LoginModel = { userName:'', password:'' };
+  loginForm: FormGroup = new FormGroup({
+    userName: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required])
+  });
+  matcher = new FormErrorStateMatcher();
 
   constructor(private accountService: AccountService, private toastr: ToastrService,
     private router: Router) {}
 
   login() {
-    this.accountService.login(this.model).subscribe({
+    const loginModel = this.loginForm.value as LoginModel;
+    this.accountService.login(loginModel).subscribe({
       next: _ => {
         this.router.navigateByUrl('');
-        this.toastr.success(`Welcome back, ${this.model.userName}!`);
+        this.toastr.success(`Welcome back, ${loginModel.userName}!`);
       },
       error: error => console.error(error)
     });
