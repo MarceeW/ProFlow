8ef561b3009b7c9c29940b9a29db1f0aad036b94
MarceeW <marcelllkadar@gmail.com>
2024-10-06 17:13:21 +0200

    Added EnumValueController for managing enum types between frontend and backend

---
+++ API/Controllers/EnumValueController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+	public class EnumValueController : BaseApiController
+	{
+		[HttpGet("{enumName}")]
+		public ActionResult<IEnumerable<string>> GetValues(string enumName) 
+		{
+			string enumTypeName = $"API.Enums.{enumName}";
+			var enumType = Type.GetType(enumTypeName);
+			
+			if(enumType == null || !enumType.IsEnum)
+				return BadRequest($"The '{enumName}' enum type doesn't exists!");
+			
+			return Ok(Enum.GetNames(enumType));
+		}
+	}
+}
