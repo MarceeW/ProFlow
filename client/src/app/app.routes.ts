1f7bad281000a580e1a02132e1e97dc843f81bdd
MarceeW <marcelllkadar@gmail.com>
2024-10-13 11:32:07 +0200

    Added routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,24 +1,26 @@
-import { roleGuard } from './_guards/role.guard';
-import { AccountPageComponent } from './account/account-page/account-page.component';
 import { Routes } from '@angular/router';
+import { RoleType } from './_enums/role-type.enum';
 import { authGuard } from './_guards/auth.guard';
 import { loginGuard } from './_guards/login.guard';
-import { LoginComponent } from './account/login/login.component';
-import { AdminMainComponent } from './admin/admin-main/admin-main.component';
-import { RegisterComponent } from './account/register/register.component';
+import { roleGuard } from './_guards/role.guard';
+import { AccountPageComponent } from './account/account-page/account-page.component';
 import { AccountSettingsComponent } from './account/account-settings/account-settings.component';
-import { ProjectListComponent } from './project/project-list/project-list.component';
-import { ProjectCreateComponent } from './project/project-create/project-create.component';
+import { LoginComponent } from './account/login/login.component';
+import { RegisterComponent } from './account/register/register.component';
+import { AdminMainComponent } from './admin/admin-main/admin-main.component';
 import { HomeComponent } from './home/home.component';
-import { TeamListComponent } from './teams/team-list/team-list.component';
+import { ProjectCreateComponent } from './project/project-create/project-create.component';
+import { ManageSprintsComponent } from './project/project-dashboard/manage-sprints/manage-sprints.component';
+import { ProductBacklogComponent } from './project/project-dashboard/product-backlog/product-backlog.component';
+import { ProjectDashboardComponent } from './project/project-dashboard/project-dashboard.component';
+import { ProjectReportsComponent } from './project/project-dashboard/project-reports/project-reports.component';
+import { ProjectSettingsComponent } from './project/project-dashboard/project-settings/project-settings.component';
+import { ProjectTimelineComponent } from './project/project-dashboard/project-timeline/project-timeline.component';
+import { ScrumBoardComponent } from './project/project-dashboard/scrum-board/scrum-board.component';
+import { ProjectListComponent } from './project/project-list/project-list.component';
 import { TeamCreateComponent } from './teams/team-create/team-create.component';
 import { TeamEditComponent } from './teams/team-edit/team-edit.component';
-import { RoleType } from './_enums/role-type.enum';
-import { ProjectDashboardComponent } from './project/project-dashboard/project-dashboard.component';
-import { ProjectTimelineComponent } from './project/project-dashboard/project-timeline/project-timeline.component';
-import { ProjectReportsComponent } from './project/project-dashboard/project-reports/project-reports.component';
-import { ProjectBacklogComponent } from './project/project-dashboard/project-backlog/project-backlog.component';
-import { ProjectSettingsComponent } from './project/project-dashboard/project-settings/project-settings.component';
+import { TeamListComponent } from './teams/team-list/team-list.component';
 
 export const routes: Routes = [
   { path: '',
@@ -68,8 +70,12 @@ export const routes: Routes = [
                 component: ProjectTimelineComponent
               },
               {
-                path: 'backlog/:id',
-                component: ProjectBacklogComponent
+                path: 'product-backlog/:id',
+                component: ProductBacklogComponent,
+              },
+              {
+                path: 'manage-sprints/:id',
+                component: ManageSprintsComponent,
               },
               {
                 path: 'reports/:id',
@@ -78,6 +84,10 @@ export const routes: Routes = [
               {
                 path: 'settings/:id',
                 component: ProjectSettingsComponent
+              },
+              {
+                path: 'scrum-board/:id',
+                component: ScrumBoardComponent
               }
             ]
           },
