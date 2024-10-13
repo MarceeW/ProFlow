39a81a09ad3b51b63b388ceba32ce40b42bc26c5
Kádár Marcell <marcelllkadar@gmail.com>
2024-10-13 17:01:16 +0200

    Fixed layout

--- client/src/app/main-nav/nav-menu/nav-menu.component.ts
+++ client/src/app/main-nav/nav-menu/nav-menu.component.ts
@@ -7,6 +7,8 @@ import { RouterModule, RouterOutlet } from '@angular/router';
 import { RoleType } from '../../_enums/role-type.enum';
 import { AccountService } from '../../_services/account.service';
 import { AsyncPipe } from '@angular/common';
+import { NotificationButtonComponent } from '../notification-button/notification-button.component';
+import { AccountMenuButtonComponent } from '../account-menu-button/account-menu-button.component';
 
 @Component({
   selector: 'app-nav-menu',
@@ -19,7 +21,9 @@ import { AsyncPipe } from '@angular/common';
     MatListModule,
     RouterOutlet,
     RouterModule,
-    AsyncPipe
+    AsyncPipe,
+    NotificationButtonComponent,
+    AccountMenuButtonComponent
   ],
   templateUrl: './nav-menu.component.html',
   styleUrl: './nav-menu.component.scss',
