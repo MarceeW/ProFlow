701620e1241f4e7d99eec8bc7c231c2a279f8c5f
MarceeW <marcelllkadar@gmail.com>
2024-10-13 02:49:36 +0200

    Format

--- API/Program.cs
+++ API/Program.cs
@@ -30,21 +30,21 @@ app.MapControllers();
 app.MapHub<NotificationHub>("hubs/notification");
 app.MapFallbackToController("Index", "Fallback");
 
- using var scope = app.Services.CreateScope();
- var services = scope.ServiceProvider;
- try
- {
- 	var context = services.GetRequiredService<DataContext>();
- 	var userManager = services.GetRequiredService<UserManager<User>>();
- 	var roleManager = services.GetRequiredService<RoleManager<Role>>();
- 	
- 	await context.Database.MigrateAsync();
- 	await Seeder.SeedUsers(userManager, roleManager);
- }
- catch (Exception ex)
- {
- 	var logger = services.GetService<ILogger<Program>>();
- 	logger?.LogError(ex, ex.Message);
- }
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+	var context = services.GetRequiredService<DataContext>();
+	var userManager = services.GetRequiredService<UserManager<User>>();
+	var roleManager = services.GetRequiredService<RoleManager<Role>>();
+
+	await context.Database.MigrateAsync();
+	await Seeder.SeedUsers(userManager, roleManager);
+}
+catch (Exception ex)
+{
+	var logger = services.GetService<ILogger<Program>>();
+	logger?.LogError(ex, ex.Message);
+}
 
 app.Run();
