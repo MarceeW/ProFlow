42ea1f55aebc0c940a83bc3d4d9575d49c6e9521
MarceeW <marcelllkadar@gmail.com>
2024-10-20 16:09:10 +0200

    Created story tile component

--- client/src/app/project/project-dashboard/story-tile/story-tile.component.ts
+++ client/src/app/project/project-dashboard/story-tile/story-tile.component.ts
@@ -1,16 +1,18 @@
-import { Component, inject, input } from '@angular/core';
+import { Component, inject, input, output } from '@angular/core';
 import { Story } from '../../../_models/story.model';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatDialog } from '@angular/material/dialog';
 import { StoryDialogComponent } from '../story-dialog/story-dialog.component';
+import { UserPictureDirective } from '../../../_directives/user-picture.directive';
 
 @Component({
   selector: 'app-story-tile',
   standalone: true,
   imports: [
     MatIconModule,
-    MatTooltipModule
+    MatTooltipModule,
+    UserPictureDirective
   ],
   templateUrl: './story-tile.component.html',
   styleUrl: './story-tile.component.scss'
@@ -18,12 +20,20 @@ import { StoryDialogComponent } from '../story-dialog/story-dialog.component';
 export class StoryTileComponent {
   readonly story = input.required<Story>();
   readonly disabled = input<boolean>(false);
+
+  readonly storyChanged = output();
+
   private readonly _dialog = inject(MatDialog);
 
   openStoryDialog() {
-    this._dialog.open(StoryDialogComponent, {
-      width: '500px',
+    const dialogRef = this._dialog.open(StoryDialogComponent, {
+      minWidth: 600,
       data: this.story()
     });
+
+    dialogRef.afterClosed().subscribe(storyChanged => {
+      if(storyChanged)
+        this.storyChanged.emit();
+    })
   }
 }
