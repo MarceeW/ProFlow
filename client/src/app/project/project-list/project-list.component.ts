2d5dc38e749f969cdd4e547e9030046b6a152755
MarceeW <marcelllkadar@gmail.com>
2024-10-20 17:36:41 +0200

    Getting only user's projects

--- client/src/app/project/project-list/project-list.component.ts
+++ client/src/app/project/project-list/project-list.component.ts
@@ -1,15 +1,15 @@
-import { Component, inject, OnDestroy, OnInit, signal, viewChild } from '@angular/core';
-import { ProjectService } from '../../_services/project.service';
-import { Project } from '../../_models/project.model';
-import { ReplaySubject, takeUntil } from 'rxjs';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatDivider } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { Router, RouterLink, RouterModule } from '@angular/router';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatTableModule } from '@angular/material/table';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatMenuModule } from '@angular/material/menu';
+import { Router, RouterModule } from '@angular/router';
+import { takeUntil } from 'rxjs';
 import { BaseComponent } from '../../_component-base/base.component';
+import { Project } from '../../_models/project.model';
+import { ProjectService } from '../../_services/project.service';
 
 @Component({
   selector: 'app-project-list',
@@ -41,7 +41,8 @@ export class ProjectListComponent extends BaseComponent implements OnInit {
 
   loadProjects() {
     this.loading.set(true);
-    this._projectService.getProjects().pipe(takeUntil(this._destroy$))
+    this._projectService.getMyProjects()
+      .pipe(takeUntil(this._destroy$))
       .subscribe(projects => {
         this.projects.set(projects);
         this.loading.set(false);
