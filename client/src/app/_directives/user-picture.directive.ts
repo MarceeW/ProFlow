cd4b050345aa7ede7b631799dac86da015ba65f2
MarceeW <marcelllkadar@gmail.com>
2024-10-23 18:32:42 +0200

    Refactor

--- client/src/app/_directives/user-picture.directive.ts
+++ client/src/app/_directives/user-picture.directive.ts
@@ -1,11 +1,11 @@
-import { AfterViewInit, Directive, ElementRef, inject, input, Renderer2 } from '@angular/core';
+import { Directive, effect, ElementRef, inject, input, Renderer2, untracked } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 
 @Directive({
   selector: 'img[user-picture]',
   standalone: true
 })
-export class UserPictureDirective implements AfterViewInit {
+export class UserPictureDirective {
   readonly userId = input.required<string>({alias: 'user-picture'});
   readonly size = input<number>(32);
 
@@ -13,7 +13,16 @@ export class UserPictureDirective implements AfterViewInit {
   private readonly _renderer = inject(Renderer2);
   private readonly _accountService = inject(AccountService);
 
-  ngAfterViewInit(): void {
+  constructor() {
+    effect(() => {
+      this.userId();
+      untracked(() => {
+        this.setPicture();
+      })
+    })
+  }
+
+  setPicture() {
     const imgElement = this._host.nativeElement;
     this._renderer.setAttribute(imgElement, 'src', this.getPictureSrc());
     this._renderer.setAttribute(imgElement, 'width', this.size().toString());
