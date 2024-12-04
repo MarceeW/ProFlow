9c6cc949c60e18d887111ba0005d7e55dc96617e
MarceeW <marcelllkadar@gmail.com>
2024-12-04 20:32:36 +0100

    Home component styling

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { BaseComponent } from '../_component-base/base.component';
+import { ProjectService } from '../_services/project.service';
+import { Project } from '../_models/project.model';
+import { takeUntil } from 'rxjs';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent extends BaseComponent {
+  readonly lastFiveProjects = signal<Project[]>([]);
+  private readonly _projectService = inject(ProjectService);
 
+  override ngOnInit(): void {
+    this._projectService.getMyProjects()
+      .pipe(takeUntil(this._destroy$))
+      .subscribe(projects => this.lastFiveProjects.set(projects.slice(0,5)));
+  }
 }
